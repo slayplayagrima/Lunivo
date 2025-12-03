199394813d7df540e85bf87e691c06ebe3494a3c
Agrima Gusain <ag.agrimagusain@gmail.com>
2025-12-03 17:46:49 +0530

    corrected fetch url in signup

--- LunivoFrontend/src/pages/Signup.jsx
+++ LunivoFrontend/src/pages/Signup.jsx
@@ -29,7 +29,7 @@ const SignUp = () => {
       }
 
       try{
-        const response=await fetch("http://localhost:5000/auth/signup",{
+        const response = await fetch("https://lunivo.onrender.com/auth/signup", {
           method:"POST",
           headers:{
             "Content-Type": "application/json",
